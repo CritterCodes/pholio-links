c1c4f9d5d4ac53ad27bea1aa27726481ea2949a1
CritterCodes <jacobaengel55@gmail.com>
2025-11-23 22:40:19 -0600

    fix: Remove /profile rewrite from middleware to allow proper redirect

--- middleware.ts
+++ middleware.ts
@@ -49,11 +49,6 @@ export function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL('/', request.url));
     }
 
-    // Rewrite /profile to /s/[subdomain]/profile on subdomains
-    if (pathname === '/profile') {
-      return NextResponse.rewrite(new URL(`/s/${subdomain}/profile`, request.url));
-    }
-
     // For the root path on a subdomain, redirect to /profile
     if (pathname === '/') {
       const url = request.nextUrl.clone();
